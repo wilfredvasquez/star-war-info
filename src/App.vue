7d94c438a90b91bc6c5a86e7aa3baf1750e90126
Wilfred Vásquez <wilfredvas@gmail.com>
2020-05-22 16:44:20 -0400

    Creación del Proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  #app
+    Header
+
+    img(alt="Vue logo" src="./assets/logo.png")
+    hello-world(msg="Welcome to Your Vue.js App")
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/components/layout/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, Header
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import './assets/scss/main.scss';
+
 </style>
+
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+  section(class="hero is-dark")
+    .hero-body
+        .container
+            h1(class="title") Primary title
+            h2(class="subtitle") Primary subtitle
+</template>
